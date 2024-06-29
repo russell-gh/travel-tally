242d5ff213a174da15d1e643ffbd3245eb881927
Zahra Vasanji <zahravasanji@hotmail.com>
2024-06-29 11:44:54 +0100

    added support for callbacks

--- src/components/FormElement.jsx
+++ src/components/FormElement.jsx
@@ -1,54 +1,84 @@
 //FormElement supports input of text, email, password, file, date, checkbox and select with a label created for all inputs.
-//Required params for all input types: id, label, type, name
+//Required params for all input types: id, label, type, name.
+//Callbacks are accepted but optional
 //Date inputs can additionally accept minDate and maxDate as a string in the format yyyy-mm-dd
 //Select inputs require an options parameter as an array of objects with a value property and name property
 //File uploads have been set to only accept image files for the profile picture
 
-const FormElement = ({ type, label, id, name, minDate, maxDate, options }) => {
+
+const FormElement = ({
+  type,
+  label,
+  id,
+  name,
+  minDate,
+  maxDate,
+  options,
+  callback,
+}) => {
   switch (type) {
     case "text":
-    case "email": //no name in email?
+    case "email":
     case "password":
       return (
         <>
-          <label for={id}>{label}:</label>
-          <input type={type} id={id} name={name} />
+          <label htmlFor={id}>{label}:</label>
+          <input type={type} id={id} name={name} onChange={callback} />
         </>
       );
     case "file":
       return (
         <>
-          <label for={id}>{label}:</label>
-          <input type={type} id={id} name={name} accept="image/*" />
+          <label htmlFor={id}>{label}:</label>
+          <input
+            type={type}
+            id={id}
+            name={name}
+            accept="image/*"
+            onChange={callback}
+          />
         </>
       );
     case "date":
       return (
         <>
-          <label for={id}>{label}:</label>
-          <input type={type} id={id} name={name} min={minDate} max={maxDate} />
+          <label htmlFor={id}>{label}:</label>
+          <input
+            type={type}
+            id={id}
+            name={name}
+            min={minDate}
+            max={maxDate}
+            onChange={callback}
+          />
         </>
       );
     case "checkbox":
       return (
         <>
-          <input type={type} id={id} name={name} />
-          <label for={id}>{label}</label>
+          <input type={type} id={id} name={name} onChange={callback} />
+          <label htmlFor={id}>{label}</label>
         </>
       );
     case "select":
       return (
         <>
-          <label for={id}>{label}:</label>
-          <select name={name} id={id}>
+          <label htmlFor={id}>{label}:</label>
+          <select name={name} id={id} onChange={callback}>
             {options.map((option) => (
-              <option value={option.value}>{option.name}</option>
+              <option key={option.value} value={option.value}>
+                {option.name}
+              </option>
             ))}
           </select>
         </>
       );
     case "button":
-      return <button type="submit">Submit</button>;
+      return (
+        <button type="submit" onChange={callback}>
+          Submit
+        </button>
+      );
   }
 };
 
