0c0e2174531cf632e452279626b7b95cc210732c
Your Name <you@example.com>
2024-07-01 12:08:38 +0100

    improved dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { currencyCodes } from "./Data/currencyCodes";
-import { expenses, travelInfo } from "./Data/fakeExpenseData";
+// import { currencyCodes } from "./Data/currencyCodes";
+// import { expenses, travelInfo } from "./Data/fakeExpenseData";
 import "./css/App.css";
 import Dashboard from "./dashboard/Dashboard";
 import { setData } from "./redux/expensesSlice";
@@ -12,26 +12,45 @@ import "./css/App.css";
 import Onboarding from "./components/Onboarding/Onboarding";
 import AddExpense from "./components/AddExpense";
 import { getCurrencyData } from "./utils/axios";
+import axios from "axios";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    getCurrencyData("GBP");
+    // getCurrencyData("GBP");
+    getApiData();
   }, []);
 
-  useEffect(() => {
-    dispatch(setData({ text: "expenses", data: expenses }));
-    dispatch(setData({ text: "travelInfo", data: travelInfo }));
-    dispatch(setData({ text: "currencyCodes", data: currencyCodes }));
-  }, [expenses, travelInfo, currencyCodes]);
+  // useEffect(() => {
+
+  // }, [expenses, travelInfo, currencyCodes]);
+
+  const getApiData = async () => {
+    {
+      const { data } = await axios.get(`currencyCodes.json`);
+      dispatch(setData({ text: "currencyCodes", data }));
+    }
+    {
+      const { data } = await axios.get(`fakeExpenseData.json`);
+      dispatch(setData({ text: "expenses", data }));
+    }
+    {
+      const { data } = await axios.get(`fakeTravelInfo.json`);
+      dispatch(setData({ text: "travelInfo", data }));
+    }
+    {
+      const { data } = await axios.get(`fakeCurrencies.json`);
+      dispatch(setData({ text: "fakeCurrencies", data }));
+    }
+  };
 
   return (
     <>
-      <Login />
+      {/* <Login />
       <Signup />
       <Onboarding />
       <Dashboard />
-      <AddExpense />
+      <AddExpense /> */}
     </>
   );
 };
