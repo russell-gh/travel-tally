8e6183bb3afdab728c2e7311f1fd2cd5d8c5b8c4
Your Name <manzi5000@hotmail.co.uk>
2024-07-02 21:01:35 +0100

    login state check nearly working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
+import { selectUser } from "../redux/onboardingSlice";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const user = useSelector(selectUser);
+  const redirect = useNavigate();
   const [formData, setFormData] = useState({});
   const onInput = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-  console.log(formData);
+  console.log(formData, user);
 
   const onSubmit = (e) => {
-    console.log("form submitted", formData);
+    // if (formData.password === state.user.password) {
+    //   console.log("form submitted", formData);
+    //   redirect("/dashboard");
+    // } else {
+    //   console.log("wrong password");
+    // }
+
+    console.log(user);
   };
 
   return (
     <div onInput={onInput}>
-      <input type="email" name="email" id="login-email" placeholder="email" />
+      <input type="email" name="email" id="email" placeholder="email" />
       <input
         type="password"
         name="password"
-        id="login-password"
+        id="password"
         placeholder="password"
       />
       <button onClick={onSubmit}>Login</button>
--- src/redux/onboardingSlice.js
+++ src/redux/onboardingSlice.js
@@ -2,25 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { trip: [], user: [], pass: [] };
 
-
 export const onboardingSlice = createSlice({
   name: "onboarding",
   initialState,
   reducers: {
-
     addTrip: (state, { payload }) => {
       state.trip = [...state.trip, payload];
     },
     addUser: (state, { payload }) => {
-      state.user = payload; //TODO check if need to spread
-    },
-    addPass: (state, { payload }) => {
-      state.pass = payload; //TODO check if need to spread
+      state.user = payload;
     },
   },
 });
 
-export const { addTrip, addUser, addPass } = onboardingSlice.actions;
+export const { addTrip, addUser } = onboardingSlice.actions;
 export const selectTrip = (state) => state.onboarding.trip; //or state.trip?
+export const selectUser = (state) => state.onboarding.user;
 
 export default onboardingSlice.reducer;
