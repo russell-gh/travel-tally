b54f74b0df6a3f5e1b1a3aa245f4ef13aa31c0aa
Sacha Uijlen <sacha-u@hotmail.com>
2024-07-07 16:16:48 +0100

    added profile to dashboard

--- src/components/dashboard/Title.jsx
+++ src/components/dashboard/Title.jsx
@@ -2,9 +2,16 @@ import { useSelector } from "react-redux";
 import { selectTrips } from "../../redux/homeSlice";
 import FilterTrip from "./filter/FilterTrip";
 import dayjs from "dayjs";
+import {
+  selectProfilePictureSrc,
+  selectUserName,
+} from "../../redux/onboardingSlice";
+import Message from "../../reusable-code/Message";
 
 const Title = ({ destination, startDate, endDate }) => {
   const trips = useSelector(selectTrips);
+  const profilePictureSrc = useSelector(selectProfilePictureSrc);
+  const userName = useSelector(selectUserName);
   startDate = dayjs(startDate);
   endDate = dayjs(endDate);
 
@@ -17,10 +24,20 @@ const Title = ({ destination, startDate, endDate }) => {
       <div className="containerTitle">
         <h1>{destination}</h1>
         <p>
-          {dayjs().isBefore(endDate) &&
-            startDate.isBefore(dayjs()) &&
-            `Travelling`}
+          {dayjs().isBefore(endDate) && startDate.isBefore(dayjs()) && (
+            <Message message="Travelling" className="travellingTitle" />
+          )}
         </p>
+        <div className="profile">
+          {profilePictureSrc && (
+            <img
+              src={profilePictureSrc}
+              alt="profile picture"
+              className="profilePicture"
+            />
+          )}
+          {userName && <Message message={userName} className="userName" />}
+        </div>
       </div>
       <FilterTrip />
     </div>
