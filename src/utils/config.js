0260e8b943b35e19bff010cc45106336dae9cf70
Sacha Uijlen <sacha-u@hotmail.com>
2024-07-08 16:24:09 +0100

    made config file

---
+++ src/utils/config.js
@@ -0,0 +1,13 @@
+// array with colors we like for the app (need to adjust for our colourpallette, need to move to utils and import)
+export const colors = [
+  "#e6194b",
+  "#3cb44b",
+  "#ffe119",
+  "#4363d8",
+  "#f58231",
+  "#911eb4",
+  "#46f0f0",
+  "#f032e6",
+  "#bcf60c",
+  "#fabebe",
+];
