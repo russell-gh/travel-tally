b1579eaa6ac94fb9af4564f77fef4bccf487c389
Zahra Vasanji <zahravasanji@hotmail.com>
2024-07-24 21:02:40 +0100

    added redux persist

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,12 +4,15 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
   // </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import onboardingReducer from "./onboardingSlice";
 import homeReducer from "./homeSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
-export const store = configureStore({
-  reducer: {
-    home: homeReducer,
-    onboarding: onboardingReducer,
-  },
+const reducers = combineReducers({
+  home: homeReducer,
+  onboarding: onboardingReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig, reducers
+);
+
+export const store = configureStore(persistedReducer);
+export const persistor = persistStore(store);
