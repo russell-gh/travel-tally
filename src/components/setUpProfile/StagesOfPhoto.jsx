9d348f4ad072ecdcf224a43c7e8261f73b375e77
Zahra Vasanji <zahravasanji@hotmail.com>
2024-07-24 19:33:52 +0100

    created axios req

--- src/components/setUpProfile/StagesOfPhoto.jsx
+++ src/components/setUpProfile/StagesOfPhoto.jsx
@@ -1,16 +1,31 @@
 import ProfileCropper from "./ProfileCropper";
 import Button from "../../reusable-code/Button";
 import { useDispatch, useSelector } from "react-redux";
-import { saveProfile } from "../../redux/onboardingSlice";
+import { saveProfile, selectUser } from "../../redux/onboardingSlice";
 import { selectProfilePictureSrc } from "../../redux/onboardingSlice";
 import { selectUserName } from "../../redux/onboardingSlice";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const StageOfPhoto = ({ profilePicture, handleCapture }) => {
   const redirect = useNavigate();
   const profilePictureSrc = useSelector(selectProfilePictureSrc);
   const userName = useSelector(selectUserName);
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
+
+  const clickHandler = async () => {
+    try {
+      await axios.post("http://localhost:6001/profile", {
+        profilePictureSrc,
+        userName,
+        userID: user.userID,
+      });
+      redirect("/onboarding");
+    } catch (e) {
+      console.log("There was an error posting to the database", e);
+    }
+  };
 
   return (
     <>
@@ -34,13 +49,7 @@ const StageOfPhoto = ({ profilePicture, handleCapture }) => {
                 redirect("/setup-profile");
               }}
             />
-            <Button
-              text="Finish"
-              animation={true}
-              onClick={() => {
-                redirect("/onboarding");
-              }}
-            />
+            <Button text="Finish" animation={true} onClick={clickHandler} />
           </div>
         </>
       )}
