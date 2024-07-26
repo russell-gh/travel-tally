e861799f62494181b75fbdd75bff6e1a5ea5230a
Your Name <you@example.com>
2024-07-26 16:33:23 +0100

    re added dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,5 +16,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
-export const store = createStore(persistedReducer);
+export const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 export const persistor = persistStore(store);
