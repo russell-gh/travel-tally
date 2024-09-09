d26199dcd252a60138c7ec2cde722e56f366865a
Zahra Vasanji <zahravasanji@hotmail.com>
2024-09-09 13:17:22 +0100

    moved images to public folder

--- src/components/Logo2.jsx
+++ src/components/Logo2.jsx
@@ -1,7 +1,7 @@
 export const Logo2 = () => {
   return (
     <div className="logo2" id="logo2">
-      <img src="../src/img/LogoOnly.png" />
+      <img src="/LogoOnly.png" />
     </div>
   );
 };
--- src/components/LogoText.jsx
+++ src/components/LogoText.jsx
@@ -1,7 +1,7 @@
 export const LogoText = () => {
   return (
     <div className="logoText" id="LogoText">
-      <img src="../src/img/LogoTextOnly.png" />
+      <img src="/LogoTextOnly.png" />
     </div>
   );
 };
